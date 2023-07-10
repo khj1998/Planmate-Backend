6b16674fb71d9de46b4427a8f6a07718b66b7598
kimhojin98@naver.com <kimhojin98@naver.com>
2023-07-10 18:05:14 +0900

    Feat : 대댓글 저장 api 작성

--- server/src/main/java/com/planmate/server/repository/CommentRepository.java
+++ server/src/main/java/com/planmate/server/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.planmate.server.repository;
 
 import com.planmate.server.domain.Comment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +15,9 @@ public interface CommentRepository extends JpaRepository<Comment,Long> {
     @Query("select c from Comment c where c.memberId = :memberId and "
           +"c.id = :commentId")
     Optional<Comment> findComment(@Param("memberId") Long memberId,@Param("commentId") Long commentId);
+
+    @Query("select c from Comment c where c.postId = :postId")
+    List<Comment> findRecentComment(@Param("postId") Long postId, Pageable pageable);
 
     List<Comment> findAllByMemberId(Long memberId);
 }
